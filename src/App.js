ab12235ef7dba12cd9a85b849054156d896c7437
Vikas Gond <vikasgond807@gmail.com>
2022-12-17 12:51:29 +0530

    Footer Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './container/Footer/Footer';
+import Carousel from './components/Carousel/Carousel';
+
 
 function App() {
   return (
     <div className="App">
+
       {/* <Navbar/> */}
+      {/* <Carousel /> */}
       <Footer/>
     </div>
   );
